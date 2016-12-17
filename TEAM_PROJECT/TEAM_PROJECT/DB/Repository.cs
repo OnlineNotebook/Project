4d744250b2668c0ad93cb372b5cf76eaabd6851d
Vygnich Roman <romachina97@yahoo.com>
2016-12-17 16:38:43 +0300

    Add ChangeItemEnd Method to Repository

--- TEAM_PROJECT/TEAM_PROJECT/DB/Repository.cs
+++ TEAM_PROJECT/TEAM_PROJECT/DB/Repository.cs
@@ -25,5 +25,23 @@ namespace TEAM_PROJECT.DB
             }
             return counter + 1;
         }
+        public void ChangeItemEnd(string value, int row, int column)
+        {
+            _Dataset.cell.Find(x => x.id_column == column && x.id_row == row).value = value;
+            if (!_Dataset.cell.Exists(x => x.id_column == column + 1 && x.id_row == row))
+            {
+                var obj = new Cell(row, column + 1, "+");
+                _Dataset.cell.Add(obj);
+            }
+            if (!_Dataset.cell.Exists(x => x.id_column == 0 && x.id_row == row + 1))
+            {
+                var obj = new Cell(row + 1, 0, "+");
+                _Dataset.cell.Add(obj);
+            }
+
+            var json = JsonConvert.SerializeObject(_Dataset);
+
+            UploadFile("ftp://vygnich.ru/public_html/teamproject/", json, login, pass, "");
+        }
     }
 }
---
+++ TEAM_PROJECT/TEAM_PROJECT/Models/PersonalTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TEAM_PROJECT.Models
+{
+    public class PersonalTable
+    {
+        public PersonalTable(string value)
+        {
+            
+        }
+    }
+}
