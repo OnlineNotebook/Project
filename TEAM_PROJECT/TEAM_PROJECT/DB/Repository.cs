2c5ec83a843616897062f424cf08fa6f3cd81717
Vygnich Roman <romachina97@yahoo.com>
2016-12-17 16:32:03 +0300

    Add GetRowsAmount Method to repository

--- TEAM_PROJECT/TEAM_PROJECT/DB/Repository.cs
+++ TEAM_PROJECT/TEAM_PROJECT/DB/Repository.cs
@@ -8,9 +8,22 @@ namespace TEAM_PROJECT.DB
     public class Repository
     {
         public RootObject _Dataset { get; set; }
-        public IEnumerable<Cell> Get() // Check
+        public IEnumerable<Cell> Get()
         {
             return _Dataset.cell.AsEnumerable();
+        }
+        public int GetRowsAmount()
+        {
+
+            int counter = 0;
+            for (int i = 0; i < _Dataset.cell.Count; i++)
+            {
+                if (_Dataset.cell[i].id_row > counter)
+                {
+                    counter = _Dataset.cell[i].id_row;
+                }
+            }
+            return counter + 1;
         }
     }
 }
