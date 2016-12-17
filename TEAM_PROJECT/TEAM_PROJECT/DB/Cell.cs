4a20e288237e7ab4553fca848500710a26fb01d7
student <student@KIRP-410-STUD15>
2016-12-17 16:16:36 +0300

    Add Cell class

---
+++ TEAM_PROJECT/TEAM_PROJECT/DB/Cell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TEAM_PROJECT.DB
+{
+    public class Cell
+    {
+        public int id_column { get; set; }
+        public int id_row { get; set; }
+        public string value { get; set; }
+        public Cell(int R, int C, string V)
+        {
+            id_row = R;
+            id_column = C;
+            value = V;
+        }
+    }
+    public class RootObject
+    {
+        public List<Cell> cell { get; set; }
+    }
+}
