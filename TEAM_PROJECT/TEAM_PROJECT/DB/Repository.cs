a7a004c1268d736fca150cb058fe5bcc6e123fb2
Vygnich Roman <romachina97@yahoo.com>
2016-12-17 17:05:51 +0300

    Finish Repository class by adding AddItem, UploadFile, ClearData, Constructor methods.

--- TEAM_PROJECT/TEAM_PROJECT/DB/Repository.cs
+++ TEAM_PROJECT/TEAM_PROJECT/DB/Repository.cs
@@ -7,6 +7,8 @@ namespace TEAM_PROJECT.DB
 {
     public class Repository
     {
+        const string login = "vygnich1";
+        const string pass = "v8jz8qP76R";
         public RootObject _Dataset { get; set; }
         public IEnumerable<Cell> Get()
         {
@@ -42,6 +44,53 @@ namespace TEAM_PROJECT.DB
             var json = JsonConvert.SerializeObject(_Dataset);
 
             UploadFile("ftp://vygnich.ru/public_html/teamproject/", json, login, pass, "");
+        }
+        public void DeleteItemEnd(int row, int column)
+        {
+            _Dataset.cell.Find(x => x.id_column == column && x.id_row == row).value = "+";
+            var json = JsonConvert.SerializeObject(_Dataset);
+            UploadFile("ftp://vygnich.ru/public_html/teamproject/", json, login, pass, "");
+        }
+
+
+        public void AddItem(PersonalTable pt) //
+        {
+            _Dataset.cell.Add(new Cell(pt.Id_Column, pt.Id_Row, pt.Value));
+            var json = JsonConvert.SerializeObject(_Dataset);
+            UploadFile("ftp://vygnich.ru/public_html/teamproject/", json, login, pass, "");
+        }
+        public static string UploadFile(string FtpUrl, string fileName, string userName, string password, string
+            UploadDirectory = "")
+        {
+            String uploadUrl = String.Format("{0}{1}/{2}", FtpUrl, UploadDirectory, "data.json");
+            FtpWebRequest req = (FtpWebRequest)FtpWebRequest.Create(uploadUrl);
+            req.Proxy = null;
+            req.Method = WebRequestMethods.Ftp.UploadFile;
+            req.Credentials = new NetworkCredential(userName, password);
+            req.UseBinary = true;
+            req.UsePassive = true;
+            byte[] data = Encoding.ASCII.GetBytes(fileName);
+            req.ContentLength = data.Length;
+            Stream stream = req.GetRequestStream();
+            stream.Write(data, 0, data.Length);
+            stream.Close();
+            FtpWebResponse res = (FtpWebResponse)req.GetResponse();
+            return res.StatusDescription;
+        }
+        public void ClearData()
+        {
+            _Dataset.cell.Clear();
+            _Dataset.cell.Add(new Cell(0, 0, "+"));
+            var json = JsonConvert.SerializeObject(_Dataset);
+            UploadFile("ftp://vygnich.ru/public_html/teamproject/", json, login, pass, "");
+        }
+
+        public Repository()
+        {
+            using (WebClient wc = new WebClient())
+            {
+                _Dataset = JsonConvert.DeserializeObject<RootObject>(wc.DownloadString("http://vygnich.ru/teamproject/data.json"));
+            }
         }
     }
 }
