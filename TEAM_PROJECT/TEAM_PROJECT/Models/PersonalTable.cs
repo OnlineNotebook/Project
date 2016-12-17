a6dd2c8ffd1ea02a513929c54e1b0d85405252b5
Egorov Roman <8481348@mail.ru>
2016-12-17 16:56:14 +0300

    Add PersonalTable class

---
+++ TEAM_PROJECT/TEAM_PROJECT/Models/PersonalTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TEAM_PROJECT.DB;
+
+
+namespace TEAM_PROJECT.Models
+{
+    public class PersonalTable
+    {
+    }
+}
