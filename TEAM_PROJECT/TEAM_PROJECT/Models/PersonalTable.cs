db41910a3409c3d06061561ecadf77b82c66c5c8
Vygnich Roman <romachina97@yahoo.com>
2016-12-17 17:01:56 +0300

    implementation of PersonalTable class

--- TEAM_PROJECT/TEAM_PROJECT/Models/PersonalTable.cs
+++ TEAM_PROJECT/TEAM_PROJECT/Models/PersonalTable.cs
@@ -9,5 +9,19 @@ namespace TEAM_PROJECT.Models
 {
     public class PersonalTable
     {
+        public int Id_Column { get; set; }
+        public int Id_Row { get; set; }
+        public string Value { get; set; }
+
+        public PersonalTable(string value)
+        {
+            Repository repo = new Repository();
+
+            Id_Column = 0;
+            Id_Row = repo.GetRowsAmount() + 1;
+            Value = value;
+        }
+        public PersonalTable()
+        { }
     }
 }
